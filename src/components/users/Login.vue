7a0a5cd6e5c14bb129bee8468b34889a45621294
Vipin <vipins110@gmail.com>
2019-11-04 16:55:36 +0530

    vue setup

---
+++ src/components/users/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about">
+        <h2>Login via Google</h2>
+    </div>
+</template>
+
+<script>
+
+import Message from '../../classes/message.class'
+console.log(new Message)
+
+export default {
+  name: 'Login'
+}
+</script>
